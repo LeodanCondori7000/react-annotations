8e75d9d4feaa5505522e0a5dafad76a361e5cf74
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-06-20 13:00:14 -0500

    controlled form added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Navbar(dropdown)-->contains a dropdown navbar
 Navbar(dropdown-customhook)-->contains a dropdown navbar   with a custom hook
 Navbar-Modal(customhook reutilization)-->contains customhook reutilization
+UncontrolledForm-->contains uncontrolled form
+Form(controlled)-->basic controlled form example
---
+++ UncontrolledForm/UncontrolledForm.jsx
@@ -0,0 +1,26 @@
+import { useRef } from "react";
+
+import "./styles.css";
+
+const UncontrolledForm = () => {
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(ref.current.value);
+  };
+
+  return (
+    <>
+      <h1>Uncontrolled Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name: <input type="text" ref={ref} />
+        </label>
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
+
+export default UncontrolledForm;
